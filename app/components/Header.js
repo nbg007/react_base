cb11e7956f141fcd0c4a2fb03e1de4926649de5e
Benjamin <li3huan1@163.com>
2017-05-01 21:39:59 +1000

    add ramda and classnames

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 
+import classNames from 'classnames/bind';
+
+import styles from '../style/index.less'
+const cx = classNames.bind(styles);
+
 class Header extends Component {
     render() {
         return (
-            <nav className="navbar navbar-default">
-                <div className="container-fluid">
-                    <div className="navbar-header">
-                        <Link className="navbar-brand" to="/">Home</Link>
-                        <Link className="navbar-brand active" to="/about">About</Link>
+            <nav className={cx('navbar', 'navbar-default')}>
+                <div className={cx('container-fluid')}>
+                    <div className={cx('navbar-header')}>
+                        <Link className={cx('navbar-brand')} to="/">Home</Link>
+                        <Link className={cx('navbar-brand', 'active')} to="/about">About</Link>
                     </div>
                 </div>
             </nav>
