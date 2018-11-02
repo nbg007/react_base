84992e52b4e6e6b6b8832cc80048ad270e947ed8
Huan Li <benjamin.li522@gmail.com>
2018-11-02 13:24:03 +1100

    Update Header.js

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router'
 
 import classNames from 'classnames/bind';
@@ -6,19 +6,15 @@ import classNames from 'classnames/bind';
 import styles from '../style/index.less'
 const cx = classNames.bind(styles);
 
-class Header extends Component {
-    render() {
-        return (
-            <nav className={cx('navbar', 'navbar-default')}>
-                <div className={cx('container-fluid')}>
-                    <div className={cx('navbar-header')}>
-                        <Link className={cx('navbar-brand')} to="/">Home</Link>
-                        <Link className={cx('navbar-brand', 'active')} to="/about">About</Link>
-                    </div>
-                </div>
-            </nav>
-        )
-    }
-}
+const Header = () => (
+    <nav className={cx('navbar', 'navbar-default')}>
+        <div className={cx('container-fluid')}>
+            <div className={cx('navbar-header')}>
+                <Link className={cx('navbar-brand')} to="/">Home</Link>
+                <Link className={cx('navbar-brand', 'active')} to="/about">About</Link>
+            </div>
+        </div>
+    </nav>
+)
 
 export default Header
